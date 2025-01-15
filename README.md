529553b9a4f432c7f9e742c80308c6edd369c07e
Brett Laptop <tri11paragon@tpgc.me>
2025-01-15 17:53:51 -0500

    it is working now

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Skyscraper Parser
 Simple python parser for getting skyscraper problems from the Caribou context website into a format which can be used in machine learning tasks
+
+requires `bs4` and `playwright` please install these using a relevant package manager or pip
+
+run with `python3 parser.py [--board_size (2-26)] [--out_file (path/to/file)]`
--- parser.py
+++ parser.py
@@ -1,63 +1,62 @@
 from bs4 import BeautifulSoup
-import subprocess
-from selenium import webdriver
-from requests_html import HTMLSession
-# from seleniumrequests import Firefox
-import time
+from playwright.sync_api import sync_playwright
+import argparse
 
 URL = "https://cariboutests.com/games/skyscrapers.php"
 
-
 def process_header_tr(tr):
-    for td in tr.find_all('td')[1:-1]:
-        print (f"header number: {td.div.span.text}")
+    return [int(td.text) for td in tr.find_all("td")[1:-1]]
 
 def process_middle_tr(tr):
     tds = tr.find_all('td')
     left = tds[0].div.span.text
     right = tds[-1].div.span.text
-    print (f"left: {left}, right: {right}")
+    return [int(left), int(right)]
 
 def process_page(source):
     soup = BeautifulSoup(source, 'html.parser')
     problem = soup.find(id='sky')
     table = problem.table.tbody
     trs = table.find_all('tr')
+    results = []
     for index,tr in enumerate(trs):
         if index == 0:
-            process_header_tr(tr)
+            results.append(process_header_tr(tr))
         elif index == len(trs)-1:
-            process_header_tr(tr)
+            results.append(process_header_tr(tr))
         else:
-            process_middle_tr(tr)
+            results.append(process_middle_tr(tr))
+    return results
 
-def download_page():
-    subprocess.run(["curl", "-X", "POST", "--user-agent", "Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0", "-d", "board_size: 8", "-o", "skyscrapers.php", URL])
-    with open("skyscrapers.php", "r+") as file:
-        content = file.read()
-    # subprocess.run(["rm", "skyscrapers.php"])
-    print(content)
-    return content
-    # options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')
-    # driver = webdriver.Chrome(options=options)
+def download_page(board_size):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto(URL, wait_until="load")
+        page.select_option("select", value=str(board_size))
+        page.click("input[type=submit]")
+        page.wait_for_load_state("load")
+        src = page.content()
+        browser.close()
+        return src
 
-    # session = HTMLSession()
-    # r = session.post(url=URL, data={"board_size": "6"}, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0"})
-    # r.html.render()
-    # print(r.html.find(id='sky'))
-    # print(r.text)
+def run():
+    parser = argparse.ArgumentParser(description='Download sky scraper problems')
 
-    # print(driver.page_source)
+    parser.add_argument('--board_size', '-b', type=int, help='Size of board to download', default=6)
+    parser.add_argument("--out_file", "-o", help="File to write the problem to", default="skyscraper_problem.txt")
 
-    # post(driver, "6")
-    # driver.get(URL)
+    args = parser.parse_args()
 
-    # src = driver.page_source
-    # print(src)
-    # driver.quit()
-
-    # return r.text
+    with open(args.out_file, "w") as file:
+        file.write("BOARD_SIZE:\t" + str(args.board_size) + '\n')
+        file.write('\n')
+        results = process_page(download_page(args.board_size))
+        for result in results:
+            for data in result:
+                file.write(str(data) + "\t")
+            file.write('\n')
 
 if __name__ == "__main__":
-    process_page(download_page())
+    run()
